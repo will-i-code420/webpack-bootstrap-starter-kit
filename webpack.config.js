102ccb9d3769bde2cdb3f1eb06ed812cd4c1a44d
jaronow <waronow123@gmail.com>
2020-02-01 17:27:27 -0700

    added rul for html loader

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,12 @@ module.exports = {
             name: '[name].[ext]'
           }
         }]
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
       }
     ]
   }
