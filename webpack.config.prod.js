1ca4133ab8c91dd870011961bb7c8104e6aca43f
jaronow <waronow123@gmail.com>
2020-02-01 23:44:21 -0700

    added image-webpack-loader to prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,6 +3,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+image-webpack-loader
 
 module.exports = {
   mode: 'production',
@@ -45,18 +46,46 @@ module.exports = {
       },
       {
         test: /\.(jpg|jpeg|gif|png|svg|webp)$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            outputPath: './images',
-            name: '[name].[ext]'
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: './images',
+              name: '[name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: false,
+                quality: 45
+              },
+              optipng: {
+                enabled: true
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: true,
+                optimizationLevel: 3
+              },
+              webp: {
+                quality: 20
+              }
+            }
           }
-        }]
+        ]
       },
       {
         test: /\.html$/,
         use: {
-          loader: 'html-loader'
+          loader: 'html-loader',
+          options: {
+            attrs: [':src', ':href']
+          }
         }
       }
     ]
